b29fd0b6960286b934c25babab010886aac95751
Jack Krause <jmkrause@iastate.edu>
2025-05-13 16:03:55 -0500

    initial commit readme

---
+++ ocaml_setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# some useful commands for ocaml (use bash for now)
+# opam init
+# eval (& opam env)
+# opam --version
+#
+#
+# # create or switch to ocaml compiler
+# opam switch show
+# # if none or not recent, pick one
+# opam switch create 4.14.0
+# eval $(opam env)
+#
+# # install core tools
+# opam install dune utop merlin ocaml-lsp-server
+
